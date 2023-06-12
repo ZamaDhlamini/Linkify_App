b49c9edad24bf19912a52737e0b82c4e5e8cbd4e
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-06-12 14:16:11 +0200

    finsihing off the rest of the services

--- src/myLinkify.Application/Services/LocationService/Dto/LocationDto.cs
+++ src/myLinkify.Application/Services/LocationService/Dto/LocationDto.cs
@@ -1,4 +1,6 @@
 ﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using myLinkify.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,17 @@ using System.Threading.Tasks;
 
 namespace myLinkify.Services.LocationService.Dto
 {
+    [AutoMap(typeof(Location))]
     public class LocationDto : EntityDto<Guid>
     {
-
+        public string Address { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Latitude { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Longitude { get; set; }
     }
 }
